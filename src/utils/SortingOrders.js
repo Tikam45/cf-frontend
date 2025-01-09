15cc00cccda66686e03fd9073d4d1ade80924ac1
Tikam45 <tikamgupta05122004@gmail.com>
2025-01-10 00:36:48 +0530

    sorting orders

--- src/utils/SortingOrders.js
+++ src/utils/SortingOrders.js
@@ -1,5 +1,12 @@
 
-export function sortOrders({data, key, order}) {
-    const result = data.sort({key: order});
+export function sortOrders({ data, key, order }) {
+    const result = data.sort((a, b) => {
+        if (order === 1) {
+            return a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0;
+        } else if (order === -1) {
+            return a[key] < b[key] ? 1 : a[key] > b[key] ? -1 : 0;
+        }
+        return 0; 
+    });
     return result;
 }
